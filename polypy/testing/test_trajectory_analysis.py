d33dd4a67d6c0cc7dc3b19da7aa39a545499da35
symmy596 <ars44@bath.ac.uk>
2018-07-04 16:25:36 +0100

    Add files via upload

--- polypy/testing/test_trajectory_analysis.py
+++ polypy/testing/test_trajectory_analysis.py
@@ -10,7 +10,7 @@ def test_distances():
     assert distance == expect
 
 def test_msd_stats():
-    data = np.genfromtxt("data/MSD.txt", dtype="float")
+    data = np.genfromtxt("polypy/testing/data/MSD.txt", dtype="float")
     a, b, c, d = ta.msd_stats(data[:,0], data[:,1], data[:,2], data[:,3], data[:,4])
     assert_almost_equal(a, 100.0)
     assert_almost_equal(b, 10.0)
@@ -25,7 +25,7 @@ def test_diffusion_coefficient():
     assert_almost_equal(d, 365)
 
 def test_system_volume():
-    data = np.genfromtxt("data/Volume.txt", dtype=float)
+    data = np.genfromtxt("polypy/testing/data/Volume.txt", dtype=float)
     expected_vol = np.array([3375.000, 4096.000, 4913.000, 5832.000, 6859.000, 8000.000, 9261.000, 10648.000, 12167.000, 13824.000])
     expected_time = np.arange(10)
     a, b = ta.system_volume(data, 10, 1)
@@ -40,7 +40,7 @@ def test_conductivity():
     assert_almost_equal(a, expected)
     
 def test_square_distance():
-    data = np.genfromtxt("data/SD.txt", dtype="float")
+    data = np.genfromtxt("polypy/testing/data/SD.txt", dtype="float")
     a = ta.square_distance(data, 1)
     b = ta.square_distance(np.array([1, 2, 3]), 0)
     expected_a = np.array([102, 408, 918, 1632, 2550, 3672, 4998, 6528, 8262, 10200])
