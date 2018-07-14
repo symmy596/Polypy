18fcb08934749109e87ce966a36ba037cd853997
symmy596 <ars44@bath.ac.uk>
2018-07-14 11:57:50 +0100

    Add files via upload

--- polypy/PolyPy.py
+++ polypy/PolyPy.py
@@ -18,11 +18,6 @@ from scipy.constants import codata
 sys.path.append('Path')
 os.environ['QT_QPA_PLATFORM']='offscreen'
 
-
-import time
-start_time = time.time()
-
-
 Atom = "BR"
 Bin = 0.1
 box = 0.1
@@ -32,43 +27,40 @@ timestep = 0.25
 
 # 1) Read Coordinates
 
-natoms, timesteps, trajectories, lv = rd.read_history("HISTORY_F", Atom)
+data = rd.read_history("HISTORY_F", Atom)
 
 # 2) Volume Calculation
 
 volume, t = ta.system_volume(lv, timesteps, timestep)
 
-# 3) Total Number of Species within a given region
+# 3) Density
+dens = ta.Density(data)
 
-plane = ta.one_dimensional_density_sb(trajectories, ul=ul, ll=ll, direction="x")
+# 4) Total Number of Species within a given region
 
-# 4) One Dimensional Density Plot
+plane = dens.one_dimensional_density_sb(ul=ul, ll=ll)
+# 5) One Dimensional Density Plot
 
-ta.one_dimensional_density(trajectories, timesteps, lv, Bin=0.1)
+dens.one_dimensional_density(Bin=0.1)
+# 6) Two Dimensional Density Plot
 
-# 5) Two Dimensional Density Plot
+dens.two_dimensional_density(box=0.1)
 
-ta.two_dimensional_density(trajectories, timesteps, lv, box=box, direction='z', log=False)
+# 6) Combined one and two dimensional density
 
-# 6) MSD Calculation with One Trajectory Sweep
+dens.two_dimensional_density(box=0.1, log=True)
+# 7) MSD Calculation with One Trajectory Sweep
 
-ta.msd(trajectories, lv, timesteps, natoms, timestep, conductivity=True, temperature=1500)
+ta.msd(data, timestep, conductivity=True, temperature=1500)
+# 8) MSD Calculation with Multiple Trajectory Sweeps
 
-# 7) MSD Calculation with Multiple Trajectory Sweeps
+ta.smooth_msd(data, timestep, runs=10, conductivity=True, temperature=1500)
+# 9) MSD Calculation Within a Specific Region of the System
 
-ta.smooth_msd(trajectories, lv, timesteps, natoms, timestep, runs=10, conductivity=True, temperature=1500)
-
-# 8) MSD Calculation Within a Specific Region of the System
-
-ta.plane_msd(trajectories, lv, timesteps, natoms, timestep, runs=10, ul=ul, ll=ll, direction="x", conductivity=False)
-
-
-# 9) MSD Calculation - Returns Plot of Atomic Diffusion Coefficient vs Average Position 
-
-ta.pmsd(trajectories, lv, timesteps, natoms, timestep)
+ta.plane_msd(data, timestep, runs=10, ul=ul, ll=ll, direction="x", conductivity=False)
 
 
-print("--- %s seconds ---" % (time.time() - start_time))
+
 
 
 
