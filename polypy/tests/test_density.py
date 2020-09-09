d6bd1bfe8311fb704e1c99bdc87acd62b47fa186
Adam Symington <ars44@bath.ac.uk>
2020-09-09 09:09:59 +0100

    Update test_density.py

--- polypy/tests/test_density.py
+++ polypy/tests/test_density.py
@@ -32,7 +32,7 @@ class TestDensity(unittest.TestCase):
         test_density.update_map([0.1, 0.1, 0.1])
         assert test_density.coords_map[1, 1, 1] == 2.0
 
-    def test_one_dimensional_density(self):
+    def test_one_dimensional_density_x(self):
         data = read.History(test_history, ['CA'])
         test_density = Density(data.trajectory, histogram_size=1.0)
         xx, yx, vol = test_density.one_dimensional_density(direction="x")
@@ -43,10 +43,58 @@ class TestDensity(unittest.TestCase):
         assert_almost_equal(xx, predicted_x)
         assert_almost_equal(yx, predicted_y)
 
-    def test_two_dimensional_density(self):
+    def test_one_dimensional_density_y(self):
         data = read.History(test_history, ['CA'])
         test_density = Density(data.trajectory, histogram_size=1.0)
-        x, y, z, vol = test_density.two_dimensional_density()
+        xx, yx, vol = test_density.one_dimensional_density(direction="y")
+
+        predicted_x = np.array([0.0, 1.0, 2.0, 3.0, 4.0,
+                                5.0, 6.0, 7.0, 8.0, 9.0])
+        predicted_y = np.zeros(10) + 1
+        assert_almost_equal(xx, predicted_x)
+        assert_almost_equal(yx, predicted_y)
+
+    def test_one_dimensional_density_z(self):
+        data = read.History(test_history, ['CA'])
+        test_density = Density(data.trajectory, histogram_size=1.0)
+        xx, yx, vol = test_density.one_dimensional_density(direction="z")
+
+        predicted_x = np.array([0.0, 1.0, 2.0, 3.0, 4.0,
+                                5.0, 6.0, 7.0, 8.0, 9.0])
+        predicted_y = np.zeros(10) + 1
+        assert_almost_equal(xx, predicted_x)
+        assert_almost_equal(yx, predicted_y)
+
+    def test_two_dimensional_density_x(self):
+        data = read.History(test_history, ['CA'])
+        test_density = Density(data.trajectory, histogram_size=1.0)
+        x, y, z, vol = test_density.two_dimensional_density(direction="z")
+        predicted_x = np.array([0.0, 1.0, 2.0, 3.0, 4.0,
+                                5.0, 6.0, 7.0, 8.0, 9.0])
+        predicted_y = np.array([0.0, 1.0, 2.0, 3.0, 4.0,
+                                5.0, 6.0, 7.0, 8.0, 9.0])
+        predicted_z = np.genfromtxt(expected_z, delimiter=",", dtype="float")
+        assert_almost_equal(x, predicted_x)
+        assert_almost_equal(y, predicted_y)
+        assert_almost_equal(z, predicted_z)
+
+    def test_two_dimensional_density_y(self):
+        data = read.History(test_history, ['CA'])
+        test_density = Density(data.trajectory, histogram_size=1.0)
+        x, y, z, vol = test_density.two_dimensional_density(direction="y")
+        predicted_x = np.array([0.0, 1.0, 2.0, 3.0, 4.0,
+                                5.0, 6.0, 7.0, 8.0, 9.0])
+        predicted_y = np.array([0.0, 1.0, 2.0, 3.0, 4.0,
+                                5.0, 6.0, 7.0, 8.0, 9.0])
+        predicted_z = np.genfromtxt(expected_z, delimiter=",", dtype="float")
+        assert_almost_equal(x, predicted_x)
+        assert_almost_equal(y, predicted_y)
+        assert_almost_equal(z, predicted_z)
+
+    def test_two_dimensional_density_z(self):
+        data = read.History(test_history, ['CA'])
+        test_density = Density(data.trajectory, histogram_size=1.0)
+        x, y, z, vol = test_density.two_dimensional_density(direction="z")
         predicted_x = np.array([0.0, 1.0, 2.0, 3.0, 4.0,
                                 5.0, 6.0, 7.0, 8.0, 9.0])
         predicted_y = np.array([0.0, 1.0, 2.0, 3.0, 4.0,
