7caf3c1d8f093686caafcd229d4c57f11c65507d
Adam Symington <ars44@bath.ac.uk>
2020-09-14 17:13:32 +0100

    Version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Adam R. Symington'
 author = 'Adam R. Symington'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5'
+release = '0.6'
 
 
 # -- General configuration ---------------------------------------------------
